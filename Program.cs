4afa051cdee82f3e6aa1565de7485a95eae1145e
Sean Castanon <castanons@usf.edu>
2021-05-15 13:47:30 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Team_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
